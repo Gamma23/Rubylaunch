c4fb1c0bd0d1a1dd46471e51e30196bd495e4429
Jordan Chen <wei2007@gmail.com>
2015-12-22 22:59:35 +0800

    methods exercises

---
+++ methods.rb
@@ -0,0 +1,37 @@
+#Exercise 1 
+def greeting(name)
+  puts "Hello #{name}"
+end 
+
+puts greeting("Jordan")
+
+#Exercise 2
+# 1. 2
+# 2. nil 
+# 3. "Joe"
+# 4. "four"
+# 5. nil 
+
+#Exercise 3 
+def multiply(a,b)
+  a * b 
+end 
+puts multiply(2,5)
+
+#Exercise 4 
+# will not print anything on the screen 
+
+#Exercise 5
+def scream(words)
+  words = words + "!!!!!"
+  return words 
+end 
+
+scream("Yipppeee")
+
+######2. nothing
+
+#Exercise 6
+# A method called calculate_product that requires two arguments,
+# but you are only providing one
+
